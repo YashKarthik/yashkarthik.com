766da41510812be5d50ec346cd1e3b5eb87de566
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2022-10-20 20:35:56 +0530

    feature: rss works; except images

--- scripts/RSSFeed.tsx
+++ scripts/RSSFeed.tsx
@@ -1,10 +1,10 @@
 import fs from "fs";
 import { Feed } from "feed";
-import { metaData } from "./Metadata";
+import { dataForRss } from "./Metadata";
 
 export const generateRssFeed = async () => {
 
-  const posts = await metaData()
+  const posts = await dataForRss()
     .then(p => p.props.sortedData);
 
   const siteURL = 'https://www.yashkarthik.xyz';
@@ -17,7 +17,7 @@ export const generateRssFeed = async () => {
   const feed = new Feed({
 
     title: "yashKarthik",
-    description: "Essays by yashKarthik on web3, programmig, and physics",
+    description: "Essays by yashKarthik on web3, programming, and physics",
     id: siteURL,
     link: siteURL,
     image: `${siteURL}/thelatenightbuilders.png`,
@@ -26,8 +26,8 @@ export const generateRssFeed = async () => {
     updated: date,
     generator: "https://github.com/jpmonette/feed",
     feedLinks: {
-      rss2: `${siteURL}/feed.xml`,
-      atom: `${siteURL}/feed.xml`,
+      rss2: `${siteURL}/rss.xml`,
+      atom: `${siteURL}/atom.xml`,
     },
 
     author,
@@ -36,13 +36,18 @@ export const generateRssFeed = async () => {
   posts.forEach(async (post) => {
 
     const url = `${siteURL}/archive/${post.shortName}`;
+
+    //const file = await import(`../pages/archive/${post.shortName}.mdx`);
+    //const content = renderToString(<file.default />);
+    //console.log(content);
+
     feed.addItem({
 
       title: post.title,
       id: url,
       link: url,
       description: post.description,
-      content: `Will be fixed soon, for now head to: ${url}`,
+      content: post.content,
       author: [author],
       contributor: [author],
       date: new Date(post.date),
@@ -50,5 +55,6 @@ export const generateRssFeed = async () => {
     });
   });
 
-  fs.writeFileSync("./public/feed.xml", feed.rss2());
+  fs.writeFileSync("./public/rss.xml", feed.rss2());
+  fs.writeFileSync("./public/atom.xml", feed.atom1());
 };
