9e812fdb5de6668c6b161c52bb60f3aba114b967
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-01-15 18:08:11 +0530

    Styling blog layout.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
     fontFamily: {
-      'serif': ['Source Serif Pro']
+      'serif': ['Source Serif Pro', ...defaultTheme.fontFamily.serif],
+      'sans': ['Inter', ...defaultTheme.fontFamily.sans],
+      'mono': ['Spline Sans Mono', ...defaultTheme.fontFamily.sans]
     }
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography')],
   darkMode: 'class'
 }
