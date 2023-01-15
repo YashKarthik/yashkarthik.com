1ee91fe995101c4ead65814d7ba43e3582ae6418
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-01-15 20:55:24 +0530

    Blog layout done 2.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,38 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      typography: (theme) => ({
+        lg: {
+          css: {
+            h1: {
+              fontSize: defaultTheme.fontSize["2xl"],
+            },
+            h2: {
+              fontSize: defaultTheme.fontSize["xl"],
+            },
+            h3: {
+              fontSize: defaultTheme.fontSize["lg"],
+            },
+          },
+        },
+        DEFAULT: {
+          css: {
+            h1: {
+              fontSize: defaultTheme.fontSize["2xl"],
+            },
+            h2: {
+              fontSize: defaultTheme.fontSize["xl"],
+            },
+            h3: {
+              fontSize: defaultTheme.fontSize["lg"],
+            },
+          },
+        },
+      }),
+    },
     fontFamily: {
-      'serif': ['Source Serif Pro', ...defaultTheme.fontFamily.serif],
+      'serif': ['Aleo', ...defaultTheme.fontFamily.serif],
       'sans': ['Inter', ...defaultTheme.fontFamily.sans],
       'mono': ['Spline Sans Mono', ...defaultTheme.fontFamily.sans]
     }
