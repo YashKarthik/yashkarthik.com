edd0b1f6c3eaa3911d3306a05e34c20c15165bc1
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-01-28 16:46:30 +0530

    Switch font.... again. Last time hopefull.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,54 +4,12 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-      typography: () => ({
-        xl: {
-          css: {
-            h1: {
-              fontSize: defaultTheme.fontSize["3xl"],
-            },
-            h2: {
-              fontSize: defaultTheme.fontSize["2xl"],
-            },
-            h3: {
-              fontSize: defaultTheme.fontSize["lg"],
-            },
-          },
-        },
-        lg: {
-          css: {
-            h1: {
-              fontSize: defaultTheme.fontSize["2xl"],
-            },
-            h2: {
-              fontSize: defaultTheme.fontSize["xl"],
-            },
-            h3: {
-              fontSize: defaultTheme.fontSize["lg"],
-            },
-          },
-        },
-        DEFAULT: {
-          css: {
-            h1: {
-              fontSize: defaultTheme.fontSize["2xl"],
-            },
-            h2: {
-              fontSize: defaultTheme.fontSize["xl"],
-            },
-            h3: {
-              fontSize: defaultTheme.fontSize["lg"],
-            },
-          },
-        },
-      }),
-    },
     fontFamily: {
-      'serif': ['Neuton', ...defaultTheme.fontFamily.serif],
+      'serif': ['Gilda Display', 'Cormorant', ...defaultTheme.fontFamily.serif],
+      'serif-styled': ['CormorantVariable', ...defaultTheme.fontFamily.serif],
       'sans': ['Inter', ...defaultTheme.fontFamily.sans],
       'mono': ['Spline Sans Mono', ...defaultTheme.fontFamily.sans],
-      'logo': ['Source Serif Pro', 'Neuton', ...defaultTheme.fontFamily.serif],
+      'logo': ['Source Serif Pro', 'Gilda Display', 'Cormorant Infant', ...defaultTheme.fontFamily.serif],
     }
 	},
 	plugins: [require('@tailwindcss/typography')],
