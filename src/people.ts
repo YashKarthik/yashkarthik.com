1c6f0e9620d7b1e01816c53f21c5cf6de75f39b4
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2024-02-21 01:12:55 -0500

    update people list

--- src/people.ts
+++ src/people.ts
@@ -132,7 +132,7 @@ export const people: People[] = [
     name: "Anson Yu",
     uni: "UWaterloo SYDE '25",
     howIKnowThem: "uwstartups, awesome website",
-    whatDoTheyDo: "builds lots of cool stuff",
+    whatDoTheyDo: "builds lots of cool stuff, recently: raytracinginoneweekend",
     links: [
       {
         name: "twitter",
@@ -160,7 +160,7 @@ export const people: People[] = [
     name: "HudZah",
     uni: "UWaterloo Math '26",
     howIKnowThem: "Socratica, AI Agent Hackathon",
-    whatDoTheyDo: "growing plants hydroponically, does ai stuff, does socratica stuff.",
+    whatDoTheyDo: "growing plants hydroponically, does ai stuff, does socratica stuff. Fun to talk to, asks great questions.",
     links: [
       {
         name: "twitter",
@@ -176,7 +176,7 @@ export const people: People[] = [
     name: "Julian Baxendale",
     uni: "UWaterloo Mech '28",
     howIKnowThem: "SJU",
-    whatDoTheyDo: "\"aesthetic and functional design\", convinced him to come to Socratica.",
+    whatDoTheyDo: "\"aesthetic and functional design\", convinced him to come to Socratica. Asks good questions.",
     links: []
   },
   {
@@ -190,5 +190,118 @@ export const people: People[] = [
         url: "https://roozbehali.com/"
       },
     ]
+  },
+  {
+    name: "Steven Gong",
+    uni: "UWaterloo SE '26",
+    howIKnowThem: "Found on Twitter, Rooz said hella chill.",
+    whatDoTheyDo: "super into robotics, watonomous, \"deep-tech\". Built, researched cool stuff. Ideas match.",
+    links: [
+      {
+        name: "stevengong.co",
+        url: "https://stevengong.co/",
+      },
+    ]
+  },
+  {
+    name: "James Lin",
+    uni: "McMaster CS (dropped out)",
+    howIKnowThem: "Found on Twitter, Brian Machado said cool guy.",
+    whatDoTheyDo: "Building a hacker house for biotech in SF.",
+    links: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/jlinbio",
+      },
+    ]
+  },
+  {
+    name: "Brian Machado",
+    uni: "UWaterloo TRON '25",
+    howIKnowThem: "AI Agent hackathon.",
+    whatDoTheyDo: "Building tools for engineering using LLMs, prev built robots n stuff. Cracked af.",
+    links: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/sincethestudy",
+      },
+    ]
+  },
+  {
+    name: "Clayton Haight",
+    uni: "UWaterloo TRON '24",
+    howIKnowThem: "Through Brian+Ivan",
+    whatDoTheyDo: "Robotics, building smth like Mobile Aloha (stanford) for their capstone project.",
+    links: [
+      {
+        name: "clayhaight.com",
+        url: "https://clayhaight.com/",
+      },
+      {
+        name: "twitter",
+        url: "https://twitter.com/clayhaight",
+      },
+    ]
+  },
+  {
+    name: "Ethan Childerhose",
+    uni: "UWaterloo TRON '24",
+    howIKnowThem: "Through Brian+Ivan+Clayton",
+    whatDoTheyDo: "Robotics, building smth like Mobile Aloha (stanford) for their capstone project.",
+    links: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/BigCrete",
+      },
+    ]
+  },
+  {
+    name: "James Hill-Khurana",
+    uni: "UWaterloo Phil+CogSci (dropped out)",
+    howIKnowThem: "Twitter",
+    whatDoTheyDo: "Working on SFCompute, been doing startupsn and interesting things for a while.",
+    links: [
+      {
+        name: "jameshk.com",
+        url: "https://jameshk.com/",
+      },
+      {
+        name: "twitter",
+        url: "https://twitter.com/jtvhk",
+      },
+    ]
+  },
+  {
+    name: "Sunnie Kapar",
+    uni: "UWaterloo CE '28",
+    howIKnowThem: "my class",
+    whatDoTheyDo: "Superb designer, fun guy.",
+    links: [
+      {
+        name: "sunniekapar.com",
+        url: "https://www.sunniekapar.com/",
+      }
+    ]
+  },
+  {
+    name: "Krish Patel",
+    uni: "UWaterloo CE '28",
+    howIKnowThem: "my class",
+    whatDoTheyDo: "Fun+smart guy. Interesting vibes. Curious. Firmware.",
+    links: [ ]
+  },
+  {
+    name: "Guhan Iyer",
+    uni: "UWaterloo CE '28",
+    howIKnowThem: "my class",
+    whatDoTheyDo: "Does Firmware. Builds stuff. Interesting guy.",
+    links: [ ]
+  },
+  {
+    name: "Rahul Kumar",
+    uni: "UWaterloo CE '28",
+    howIKnowThem: "my class",
+    whatDoTheyDo: "Does lots of diff stuff; AI, Three.js, full-stack, competetive-prog god.",
+    links: [ ]
   }
 ];
